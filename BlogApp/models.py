75b9b267ceafa43e42eeca990c7a46302d41d4ca
Gaston Rios <gastonrios32@gmail.com>
2022-01-15 20:02:35 -0300

    Creacion Db, Correcion en models.py, Ajustes para urls.py

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Lector(models.Model):
     nombre=models.CharField( max_length=50)
     email=models.CharField( max_length=50)
-    fechaIngreso=models.models.DateField( auto_now=False, auto_now_add=False)
+    fechaIngreso=models.DateField( auto_now=False, auto_now_add=False)
     def __str__(self):
             return f'{self.nombre}'
     
 class Publicacion(models.Model):
     nombre=models.CharField( max_length=50)
-    fechaCreacion=models.models.DateField( auto_now=False, auto_now_add=False)
+    fechaCreacion=models.DateField( auto_now=False, auto_now_add=False)
     descripcion=models.CharField( max_length=500)
     def __str__(self):
         return f'{self.nombre}'    
